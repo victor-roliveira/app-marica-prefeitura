3581e03b52216dc9535f5d8aefe561409aa9c671
victor-roliveira <victorramos123@outlook.com>
2026-01-15 11:59:29 -0300

    fix: adjust error in login url

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -21,7 +21,7 @@ function loadSession() {
 }
 
 async function login({ email, password }) {
-    const { data } = await api.post("/auth/login", { email, password })
+    const { data } = await api.post("auth/login", { email, password })
 
     const accessToken = data.token ?? data.accessToken
     const userPayload = data.user ?? null
